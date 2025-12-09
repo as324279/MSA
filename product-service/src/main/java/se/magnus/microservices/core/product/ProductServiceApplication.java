a2a781d5215d83b047e0153b45dddf083d82553f
as324279 <as324275@naver.com>
2025-12-09 15:11:31 +0900

    chapter04

--- product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
+++ product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
@@ -2,8 +2,13 @@ package se.magnus.microservices.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "se.magnus.microservices.core.product",
+    "se.magnus.util.http"   // util 모듈 스캔 추가
+})
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
